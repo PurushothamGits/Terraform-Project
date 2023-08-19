f22f3668d5610afd6b4ba80d40f4b6c98005f1f2
root <root@ip-172-31-38-164.ec2.internal>
2023-08-19 18:50:31 +0000

    created the new folder and files

---
+++ ec2/main.tf
@@ -0,0 +1,18 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "Jenkins-Terraform"
+
+  instance_type          = var.Instance_Type
+  key_name               = "MyDevops_NV"
+  monitoring             = false
+  vpc_security_group_ids = ["sg-0969e41e0d5891767"]
+  subnet_id              = "subnet-0bc5686fe387a6256"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+    project     = "My Project"
+    owner       = "Devops"
+  }
+}
---
+++ ec2/var.tf
@@ -0,0 +1,4 @@
+variable "Instance_Type"{
+        description  = "Please provide the Instance Type"
+        type         = string
+}
